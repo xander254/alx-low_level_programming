098ba27b3b1cb40a3b36611532ca61b286e9b6b9
xander254 <alexkinyanjui356@gmail.com>
2023-02-19 05:17:26 -0800

    vis n positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ *main- start point
+ * 
+ *Return: always 0
+ */
 int main(void)
 {
 	int n;
