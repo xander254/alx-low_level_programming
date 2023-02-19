b73343764fdb422c99802ac5f97e9f9bdc89ceff
xander254 <alexkinyanjui356@gmail.com>
2023-02-19 09:09:04 -0800

    list them numbers but wit commas and spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base
- * 10 starting from 0, followed by a new line
+ * main - prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-	{
+		if (n != 57)
+		{
 			putchar(',');
 			putchar(' ');
 		}
@@ -20,3 +20,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
