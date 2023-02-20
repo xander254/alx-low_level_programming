cdc648ad1fe9bd4d7d35e8fd478e4782e25c06cd
xander254 <alexkinyanjui356@gmail.com>
2023-02-20 05:52:14 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int n;
 	char x;
 
-	for (n = 0; n < 10; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
@@ -18,5 +18,6 @@ int main(void)
 	{
 		putchar(x);
 	}
+	putchar('\n');
 	return (0);
 }
