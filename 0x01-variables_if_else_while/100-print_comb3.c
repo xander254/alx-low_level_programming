47620a2775cdb1414a6cd5e2df2c8bc2fc5bac50
xander254 <alexkinyanjui356@gmail.com>
2023-02-20 07:13:57 -0800

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = '0'; n = '9'; n++)
+	{
+		for (x = '0'; x = '9'; x++)
+		{
+			putchar(n);
+			putchar(x);
+			if (x < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
