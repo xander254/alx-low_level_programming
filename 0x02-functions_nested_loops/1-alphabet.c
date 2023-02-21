52bbba8be059abf8a95284026fafd84a1e8ee350
xander254 <alexkinyanjui356@gmail.com>
2023-02-21 07:19:16 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+_putchar(void)
+char (a)
+{
+	void print_alphabet(void);
+	{
+		 char (a == 'a', a <= 'z', a++)
+	 	{
+			 _putchar(a);
+			 _putchar('\n')
+		 }
+	}
+	return (0);
+}
