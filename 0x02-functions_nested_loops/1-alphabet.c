466800e0397f4cf14083819d66b7aa5e0c9faf55
xander254 <alexkinyanjui356@gmail.com>
2023-02-22 09:29:53 -0800

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,10 @@ void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 
 
-		_putchar('\n');
+			_putchar('\n');
 }
 
