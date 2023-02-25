da0a7cb4caac32d9dc530f3caa53974ff8daf7fd
xander254 <alexkinyanjui356@gmail.com>
2023-02-25 00:01:07 -0800

    alpha 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
+	char c;
 	int i;
 
 
@@ -16,11 +16,11 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(c);
+			c++;
 		}
 			_putchar('\n');
 			i++;
