6f699a6561118418bd585dd280f640a90bafff47
xander254 <alexkinyanjui356@gmail.com>
2023-02-25 06:13:20 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_numbers - print 0-9
+* return: void
+*/
+void print_numbers(void)
+{
+	for ('a' >= 48 && 'a' < 10)
+	{
+	_putchar('a');
+	}
+	return (0);
+}
+void main();
