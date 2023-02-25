b08d21461ccdf2f29cee27eb2ef9ae9a1fd07eeb
xander254 <alexkinyanjui356@gmail.com>
2023-02-24 23:27:55 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+
+	if ("_isupper(c)");
+	{		
+		return (1);
+		putchar('\n');
+	}
+
+	else if
+	{
+		printf('\n');
+		return (0);
+	}
+}
