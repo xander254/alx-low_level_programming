2a6abe7fd76e6811ed843d6017914174fdfe09c7
xander254 <alexkinyanjui356@gmail.com>
2023-02-25 05:58:04 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,15 +2,11 @@
 
 /**
 *mul - multiply two int
-*@:a first int
-*@:b second int
+*@: a first int
+*@: b second int
 *Return: addition of the multiplicattion
 */
 int mul(int a, int b)
 {
-	for (a = 3 && b = 1)
-	{
-		printf("%d\n", mul(a, b));
-	}
-	return (0);
+	return (a * b);
 }
