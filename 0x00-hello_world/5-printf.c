cf4a6afd1e2a9b6e2ae00117528d0fdf3d8b2212
alex kinyanjui <alexkinyanjui356@gmail.com>
2023-10-08 08:21:43 -0700

    use printf to send a string to stdo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - this is the main function
+  * return: Always 0 (Success)
+  * printf - print chars in stdo
+  */
+main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
