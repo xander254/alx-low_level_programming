da819f97162acded4ebfd8830bc5621dba8191f5
alex kinyanjui <alexkinyanjui356@gmail.com>
2023-10-08 08:21:01 -0700

    using the puts send a string to stdo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+ /**
  * main - Entry point
  * Return: Always 0 (Success)
  * puts - to print a string
  */
-int main(void)
+main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
