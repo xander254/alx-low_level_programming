2e386ac0be747599cc15438128599d2d56a4f64f
alex kinyanjui <alexkinyanjui356@gmail.com>
2023-10-08 08:25:01 -0700

    putchars using put and printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
   * return: Always 0 (Success)
   * printf - print chars in stdo
   */
-main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
