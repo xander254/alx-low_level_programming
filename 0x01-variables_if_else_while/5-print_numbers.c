63c76b9211be1bfbfae341db9e16f503a8fdfd6b
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-12 14:09:57 +0300

    print 1to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  main -Entry point
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
