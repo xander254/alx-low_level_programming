94368609f30f6dc562d1611a390d8ebb278f0137
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-15 01:54:49 +0300

    print the alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include  "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	int a;
+	char c;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar('c');
+	}
+	_putchar('\n');
+}
