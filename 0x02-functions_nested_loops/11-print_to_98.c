a080b229013d262ff692c37e96b1ce9485fe92e3
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-17 22:45:17 +0300

    print n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,13 +21,13 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d", n);
+				printf("%d, ", n);
 			}
 		}
 	}
 	else
 	{
-				for (; n >= 98; n++)
+				for (; n >= 98; n--)
 				{
 					if (n == 98)
 					{
