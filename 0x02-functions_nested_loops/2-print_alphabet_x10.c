fde05aef23d5df28728a1d55ff8172ec19fc7c33
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-17 21:02:57 +0300

    print the alphabet x 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,14 @@
 
 void print_alphabet_x10(void);
 {
-	int a;
 	char c;
-	a = 0;
+	int a = 0;
 
-	while (a < 10)
+	while (a <= 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar('c');
+			_putchar(c);
 		}
 	}
 	_putchar('\n');
