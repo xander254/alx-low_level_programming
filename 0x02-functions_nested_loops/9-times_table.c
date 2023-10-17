ac4080aa223c5039048f2f6611a36aadd4b17a88
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-17 21:49:34 +0300

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 {
 	int a, b, c, d, e;
 
-	for (a = 0; x <= 9; x++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -36,4 +36,5 @@ void times_table(void)
 			}
 		}
 		_putchar('\n');
-	}
+}
+}
