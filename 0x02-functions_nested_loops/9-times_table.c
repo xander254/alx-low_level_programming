39bc98813a23ea75be5159bbd49d6e903d35ae1a
Alex Kinyanjui <Alexkinyanjui356@gmail.com>
2023-10-17 21:44:38 +0300

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - print the times table starting with 0
+ *
+ * Return - empty output
+ */
+
+void times_table(void)
+{
+	int a, b, c, d, e;
+
+	for (a = 0; x <= 9; x++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(e + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
