5da3889c23951c964b666d21bf7142d4ed84d9b4
xander254 <alexkinyanjui356@gmail.com>
2023-10-26 21:09:59 +0300

    is n negative/positive/0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there
+/**
  * main - Entry point
  * Return: Alwyas 0
- * */
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
