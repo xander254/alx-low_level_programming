1e858b7d5a2875e6bc4ad37ba428079977de20ef
xander254 <alexkinyanjui356@gmail.com>
2023-10-28 18:14:44 +0300

    check is char c is upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if char is uppercase
+ * @c: cahr to be checked
+ * Return: 1 if upper else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
