9d8152c9aae97bb0f59f7fafb8ffa8f34f11ea66
xander254 <alexkinyanjui356@gmail.com>
2023-11-09 02:35:38 +0300

    print a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts: - function to print a string
+ * @str: -pointer to the string
+ * Return: string and a '\n'
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
