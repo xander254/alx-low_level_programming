e178b3540fd583be6e60b02012d3c682b4c0480b
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 01:55:38 +0300

    print a..zA..Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** Print the alpHABET
+  * main - the entry point of my algorithm
+  * putchar - print values
+  * Return: always 0
+  */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 97; x <= 122; x++)
+	{
+		putchar(x);
+	}
+	for (y = 65; y <= 90; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
