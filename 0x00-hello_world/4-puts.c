bedf50526e7f8e6ba77d17a85865a681fa798f90
xander254 <alexkinyanjui356@gmail.com>
2024-06-21 03:22:07 +0300

    print to stdio using s commsnd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
 int main(void)
 
 {
-	char* string = "\"Programming is like building a multilingual puzzle";
-
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
