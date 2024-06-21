2860ffcb2269c4417966be291a0686522c14c594
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 02:20:44 +0300

    generate numbers from 0..9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *printf - print values
+  *Return: 0
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
+
