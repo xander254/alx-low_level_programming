d285056560efb50f3473efc12b20c7ab54a5aafe
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 00:31:20 +0300

    a program that checks if a variable n is negative, sero of positive and displays it

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("Last digit of %d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf(" Last digit of %d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("Last digit of %d is negative\n", n);
+		printf(" %d is negative\n", n);
 	}
 	return (0);
 }
