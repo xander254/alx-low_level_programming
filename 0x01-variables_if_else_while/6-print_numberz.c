8029b7accdd9792e79957e1f2c93d15a53cdf9b0
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 02:39:07 +0300

    print 0..9 using putchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *putchar - print character
+  *Return: 0
+  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 40; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
