35c25c7e178abc90656c92a510b612f7f3d32b6f
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 01:12:40 +0300

    a program that checks if the variable n is greater than 5 or o0 or if the number is less than 6 and not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n %10);
 	}
 	else if (n < 6 && n != 0)
 	{
