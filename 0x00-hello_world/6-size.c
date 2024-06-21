a9de8cc7b1a16141e244f0fc7034c5bfdf42671d
xander254 <alexkinyanjui356@gmail.com>
2024-06-21 03:19:35 +0300

    print he size of int..char..int..long int to stdo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s), sizeof(char)\n");
 	printf("Size of an int: %lu byte(s), sizeof(int)\n");
 	printf("Size of a long int: %lu byte(s), sizeof(longint)\n");
-	printf("Size of a long long int: %lu byte(s), sizeof(longlongint)\n)";
+	printf("Size of a long long int: %lu byte(s), sizeof(longlongint)\n");
 	printf("Size of a float: %lu byte(s)\n");
-
 	return (0);
 }
