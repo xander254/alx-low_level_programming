f178eb56b5d574b686eb86d0ddf15635439cdb27
xander254 <alexkinyanjui356@gmail.com>
2024-06-22 03:15:32 +0300

    print hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *putchar- print to std output
+  *Return: 0
+  */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b <= 102; a++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
