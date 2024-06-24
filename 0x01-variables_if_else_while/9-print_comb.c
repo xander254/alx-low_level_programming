08c98f32e3de57516dd561fb7aacdc1d0a420267
xander254 <alexkinyanjui356@gmail.com>
2024-06-25 00:38:28 +0300

    print "0, 1, 2, 3, 4, 5, 6, 7, 8, 9" using 4 putchar functions only

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@ int main(void)
 {
 	int a;
 
-	for (a = 48; a <= 56;)
+	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
-		putchar(',');
-		putchar(' ');
-		a++;
+		if (a < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
-	putchar(57);
 	putchar('\n');
 	return (0);
 }
