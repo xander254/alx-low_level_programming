4267013f9709ec4081e0d7cf4ba9a6de2e2a0b0c
xander254 <alexkinyanjui356@gmail.com>
2024-06-25 00:49:47 +0300

    this is the v1 of the solution I will mofiy it as I go

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *putchar - print to std o
+  *Return: 0
+  */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
