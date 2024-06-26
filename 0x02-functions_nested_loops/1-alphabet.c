dc5a5eeee0362811c5ace23442eb272a26ee4c5f
xander254 <alexkinyanjui356@gmail.com>
2024-06-26 14:10:57 +0300

    print the alphabet using putchar in  the function print_alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *main - the entry pint of my program
- *_putchar - print to the standard output
+ *print_alphabet - print the alphabet
+ *_putchar - print to std output
  *Return: 0
  *@a - charcter to be printed
  */
@@ -10,9 +10,10 @@
 void print_alphabet(void)
 {
 	int a = 97;
+
 	for (a = 97; a <= 122; a++)
 	{
-		putchar(a);
+		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
