fb7ca84ac60a3352edb62860cefc8f7b479fabf9
xander254 <alexkinyanjui356@gmail.com>
2024-06-26 15:22:59 +0300

    find if character is lower. Return 1 if lower else return 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - check if a character is lower
+ *@r - char to be checked
+ *Return: 1 if lowercase else 0
+ */
+
+int _islower(int r)
+{
+	_islower
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
