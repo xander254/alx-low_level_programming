6102074de399065f65139c0957417eff6da53623
xander254 <alexkinyanjui356@gmail.com>
2024-06-27 01:24:30 +0300

    find the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _abs - find the absolute value of an int
+  * @r: integer to be checked
+  * Return: 0
+  */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = (-1) * r;
+		return (0);
+		_putchar('\n');
+	}
+	else if (r == 1)
+	{
+		r = (-1) * r;
+		return (1);
+		_putchar('\n');
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
