3f2e8ab9af80d1a7914fdad6b4e4d31d256d7a34
xander254 <alexkinyanjui356@gmail.com>
2024-06-29 02:49:59 +0300

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+  *print_numbers - A program to print 0-9
+  *
+  *Return 0
+  *
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
