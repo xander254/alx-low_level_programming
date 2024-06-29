90bea1f284677ad9eeb5f81d468cce447a092b86
xander254 <alexkinyanjui356@gmail.com>
2024-06-29 04:56:21 +0300

    FIZZ BUZZ in C

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ", i);
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ", i);
+			printf("Buzz ");
 		}
 		else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz ", i);
+			printf("FizzBuzz ");
 		}
 		else
 		{
