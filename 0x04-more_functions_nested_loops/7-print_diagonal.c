2c46069888d20456033ac9756c2b106aa2be820e
xander254 <alexkinyanjui356@gmail.com>
2024-06-29 05:55:03 +0300

    print diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *print_diagonal - print a dialogue
+  *Return: 0
+  *@n: number of diagonals
+  */
+
+void print_diagonal(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+}
+
