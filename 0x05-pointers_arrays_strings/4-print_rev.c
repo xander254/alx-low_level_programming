d6bb03d526058eca5c4a990b1e5fa9b993156e2f
xander254 <alexkinyanjui356@gmail.com>
2024-07-04 00:16:42 +0300

    prit the reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *print_rev - print in reverse
+  *Return: 0
+  *
+  */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+	char *end = s;
+
+	while (*end != '\0')
+	{
+		length++;
+		end++;
+	}
+	for (i = length -1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
