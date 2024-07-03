efd26b7ea8ad0b0df8b0cc51c56e10fb5f7bd179
xander254 <alexkinyanjui356@gmail.com>
2024-07-03 16:27:07 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *_strlen - check the length of a string
+  *Return: 0
+  *
+  */
+int _strlen(char *s)
+{
+	strlen(&s);
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  *_puts - print a strng to stdout
+  *Retun: 0
+  *
+  */
+void _puts(char *str)
+{
+
