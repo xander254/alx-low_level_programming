7373aaae4d3f3842be512ad95ba742a2965de6d2
xander254 <alexkinyanjui356@gmail.com>
2024-07-03 14:03:26 +0300

    print 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,33 +7,25 @@
 
 void jack_bauer(void)
 {
-	int a;
-	int b;
-	int c;
-/*	int d;
-*/
-	for (a = 48; a <= 50;)
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
 	{
-		_putchar(a);
-		a++;
-	for (b = 48; b <= 51;)
+	for (b = 0; b <= 3; b++)
 	{
-		_putchar(b);
+	if ((a < 1 && b < 9) || (a <= 2 && b <= 3))
+		for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
 		_putchar(58);
-		b++;
-	for (c = 48; c <= 53;)
-	{
-		_putchar(c);
-		c++;
-		/**
-				for (d = 48; d <= 57;)
-				{
-				_putchar(c);
-				*/
-	_putchar('\n');
-			
-			
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
 	}
 	}
 	}
 	}
+}
