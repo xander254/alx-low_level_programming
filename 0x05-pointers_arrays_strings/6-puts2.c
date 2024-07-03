145aefdecd354044cc99f6c37b2f422d3949e0ff
xander254 <alexkinyanjui356@gmail.com>
2024-07-04 01:35:53 +0300

    print 0-9

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *puts2 - print to std out
+  *Return:0
+  *@str: string to be used
+  */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		str++;
+		putchar(*str);
+	}
+	putchar('\n');
+}
