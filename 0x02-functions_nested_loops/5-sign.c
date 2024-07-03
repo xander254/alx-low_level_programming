42ffb1c8c505b20909ed4f819edb8d1a417ce392
xander254 <alexkinyanjui356@gmail.com>
2024-07-03 13:37:24 +0300

    print the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int r)
 		_putchar(43);
 		return (1);
 	}
-	else if (r == 0)
-	{
-		_putchar(48);
-		return (0);
-	}
-	else
+	else if (r < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
 
