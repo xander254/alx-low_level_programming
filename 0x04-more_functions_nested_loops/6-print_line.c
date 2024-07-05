9deb494d705b5cb9eb29ae83ef2fb58122a1c644
xander254 <alexkinyanjui356@gmail.com>
2024-07-05 14:53:35 +0300

    print a line x times

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,18 @@
 
 void print_line(int n)
 {
+	int i;
 
-		if (n <= 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar(95);
+			_putchar('_');
 		}
 		_putchar('\n');
+	}
 }
