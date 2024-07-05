2e572b1b9496b5edc73b93761b9f64db1eb64a32
xander254 <alexkinyanjui356@gmail.com>
2024-07-05 22:50:30 +0300

    hotfix: fixed betty coding style line length

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,19 +14,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last_d = n % 10;
 
-	if (last_digit > 5)
+	if (last_d > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	}
-	else if (last_digit == 0)
+	else if (last_d == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is 0\n", n, last_d);
 	}
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	}
 	return (0);
 }
