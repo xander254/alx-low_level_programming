90537d6fba36d77d337e403e4ec6fc5daff1fe34
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 01:08:23 +0300

    factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 
- 
+/**
+  *primeFactors - find prime factors of n
+  *main - the entry point of our program
+  *Return: 0
+  *@n: number to be used
+  */
+
 void primeFactors(long int n)
 {
-    int i; 
-	while (n % 2 == 0) {
-        printf("%d ", 2);
-        n = n / 2;
-    }
-    for (i = 3; i * i <= n; i = i + 2) {
-        while (n % i == 0) {
-            printf("%d ", i);
-            n = n / i;
-        }
-    }
- 
-        printf("%ld ", n);
+	int i;
+
+	while (n % 2 == 0)
+	{
+		printf("%d ", 2);
+		n = n / 2;
+	}
+	for (i = 3; i * i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			printf("%d ", i);
+			n = n / i;
+		}
+	}
+	printf("%ld ", n);
 }
- 
-int main()
+
+int main(void)
 {
-    long int n = 612852475143;
-    primeFactors(n);
-    return 0;
+	long int n = 612852475143;
+
+	primeFactors(n);
+	return (0);
 }
 
