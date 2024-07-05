17e81a1e3c6d37c28fa1eb6942a3e19cc5e40bf0
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 00:35:48 +0300

    print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,21 @@
 
 void print_square(int size)
 {
-	size = 0;
+	int i, j;
 
 	if (size < 0)
 	{
 		_putchar('\n');
 	}
+	else 
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+		}
+	}
+	_putchar('\n');
 }
