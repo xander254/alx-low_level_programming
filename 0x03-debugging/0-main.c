b29a9197aac01e6f4f3ff81fda09673685c0f73c
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 11:39:44 +0300

    debugg the code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,15 +4,15 @@
 
 /**
   * positive_or_negative - find if an int is positive or negative
-  * printf - print to the standard output
   * Return: always 0
-  *@n: int to be checked
+  * @n: int to be checked
   */
 
 void positive_or_negative(int n)
 {
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
