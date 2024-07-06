f9795747933979a936aafb1e36440075f50e480f
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 11:59:23 +0300

    find if n is negative/positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,26 +1,15 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
 
 /**
-  * positive_or_negative - find if an int is positive or negative
-  * Return: always 0
-  * @n: int to be checked
-  */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int i)
 {
+        int i;
 
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
