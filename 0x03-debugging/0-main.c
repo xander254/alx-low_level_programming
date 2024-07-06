967445848b721cba060c9193ba87e996133ce7ec
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 11:41:12 +0300

    debugg the code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,21 +8,21 @@
   * @n: int to be checked
   */
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
 }
