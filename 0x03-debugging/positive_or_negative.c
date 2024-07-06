e64cf8e2e5ac45b07894d95c14131804ce60e90d
xander254 <alexkinyanjui356@gmail.com>
2024-07-06 12:09:17 +0300

    find if i is +/-

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,28 +3,23 @@
 #include <stdio.h>
 
 /**
-  * main - the entry point of my algorithm
-  * printf - print to the standard output
+  * positive_or_negative - find if i is positive/negative
+  * @i: param to be checked
   * Return: always 0
   */
-
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is zero\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative\n", i);
 	}
-	return (0);
 }
