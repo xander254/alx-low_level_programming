f2d1f07ef4d5e33ba1fc9186b092444365cea786
xander254 <alexkinyanjui356@gmail.com>
2024-07-09 15:54:00 +0300

    capitalize what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,13 +11,24 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	if (str == NULL)
+	{
+		return NULL;
+	}
+
+	if (isalpha((unsigned char)str[i]))
+	{
+		str[i] = toupper((unsigned char)str[i]);
+	}
 	while (str[i] != '\0')
 	{
-		putchar(toupper(*str));
+		if ((str[i] == ',' || str[i] == ' ' || str[i] == '.' || str[i] == ';' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') && isalpha((unsigned char)str[i + 1]))
+		{
+			str[i + 1] = toupper((unsigned char)str[i + 1]);
+		}
 		i++;
 	}
-	putchar('\n');
-	return (0);
+	return (str);
 }
 
 
