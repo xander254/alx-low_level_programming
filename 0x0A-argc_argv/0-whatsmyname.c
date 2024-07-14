3ce11c2d68fa5ad8426b9d7afa7cf149a3dbf427
xander254 <alexkinyanjui356@gmail.com>
2024-07-14 17:09:36 +0300

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+  * main -entry point of oyr program
+  * A program that prints its name
+  * return: 0
+  */
+
+int main (int argc, char * argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
