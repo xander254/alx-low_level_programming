0d4278ca52fe6f4e33dbb6c8113edea0441ff34d
xander254 <alexkinyanjui356@gmail.com>
2024-07-16 18:22:51 +0300

    cout the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,16 +3,44 @@
 #include <stdlib.h>
 
 /**
-  * main - minimum number of coins to make an amount of money
+  * countCoins - minimum number of coins to make an amount of money
+  * @number: the number of cents to be given back.
+  * Return: 0 if successful else return 1.
+  */
+
+void countCoins(int number)
+{
+	int count_25 = 0, count_10 = 0, count_5 = 0;
+	int count_2 = 0, count_1 = 0;
+
+	count_25 = number / 25;
+	number = number % 25;
+
+	count_10 = number / 10;
+	number = number % 10;
+
+	count_5 = number / 5;
+	number = number % 5;
+
+	count_2 = number / 2;
+	number = number % 2;
+
+	count_1 = number / 1;
+	number = number % 1;
+
+	printf("%d\n", count_25 + count_10 + count_5 + count_2 + count_1);
+}
+
+/**
+  * main - Entry poit of our program
   * @argc: argument count
   * @argv: argument vector
-  * Return: 0 if successful else return 1.
+  * Return: 0 if successful else 1
   */
 
 int main(int argc, char *argv[])
 {
 	int number;
-	int count_25;
 
 	if (argc != 2)
 	{
@@ -26,9 +54,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	count_25 = number / 25;
-	number = number % 25;
-	printf("%d\n", count_25);
+	countCoins(number);
 	return (0);
 }
 
