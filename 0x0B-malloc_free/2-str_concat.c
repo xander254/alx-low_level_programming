2502cb2b30b48c71691a77e38c5d32e47f1c4fe1
xander254 <alexkinyanjui356@gmail.com>
2024-07-18 16:39:33 +0300

    concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,21 +28,20 @@ char *str_concat(char *s1, char *s2)
 	length1 = strlen(s1);
 	length2 = strlen(s2);
 
-	s3 = malloc(sizeof(char) * (length1 + length2 + 1));
+	s3 = malloc(sizeof(char) * (length1 + length2 + 2));
 
 	if (s3 == NULL)
 	{
 		return (NULL);
 	}
-	for (index = 0; index <= length1; index++)
+	for (index = 0; index < length1; index++)
 	{
 		s3[index] = s1[index];
 	}
 	for (index2 = 0; index2 <= length2; index2++)
 	{
-		s3[index] = s2[index2];
+		s3[index + index2] = s2[index2];
 	}
-	s3[index] = '\0';
+	s3[length1 + length2] = '\0';
 	return (s3);
 }
-
