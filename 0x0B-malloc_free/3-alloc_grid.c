51737c31b453cdab9151ad7a411d46f36efac196
xander254 <alexkinyanjui356@gmail.com>
2024-07-18 18:41:14 +0300

    alloctate an array grid with values provided

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * alloc_grid - return a ptr to a 2d array of ints
+  * @width: width of the grid to be innnitialized
+  * @height: height of the grid to be innitialized
+  * Return: pointer to a 2d array of integers else NULL
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int) * (height * width));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int));
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[j] = malloc(sizeof(int));
+			if (grid[j] == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
+
