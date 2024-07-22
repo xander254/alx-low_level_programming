a44280bf97fb6405697260a2a64d85463a276600
xander254 <alexkinyanjui356@gmail.com>
2024-07-23 01:49:16 +0300

    A program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - a program that prints tha name of the file it
+  * was complied from.
+  * Return: Always 0
+  */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}
