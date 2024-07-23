33eb60ec2fd1ad2313c10cee621b31216e4f5b09
xander254 <alexkinyanjui356@gmail.com>
2024-07-24 00:44:32 +0300

    fix: loops, a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,23 +11,26 @@
 
 void print_dog(struct dog *d)
 {
-	if ((*d).name == NULL)
-	{
-		printf("%s:(nil)\n", (*d).name);
-	}
-	else if ((*d).age == 0)
-	{
-		printf("%f:(nil)\n", (*d).age);
-	}
-	else if ((*d).owner == 0)
-	{
-		printf("%s:(nil)\n", (*d).owner);
-	}
 	if (d == NULL)
 	{
 		printf("NULL");
 	}
-	printf("Name: %s\n", (*d).name);
+	if ((*d).name == NULL)
+	{
+		printf("%s:(nil)\n", (*d).name);
+	}
+	else
+	{
+		printf("Name: %s\n", (*d).name);
+	}
 	printf("Age: %f\n", (*d).age);
-	printf("Owner: %s\n", (*d).owner);
+
+	if ((*d).owner == 0)
+	{
+		printf("%s:(nil)\n", (*d).owner);
+	}
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
+	}
 }
