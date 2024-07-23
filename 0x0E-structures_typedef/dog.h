e3fba680f55bffe9b31580656bd6d757eca90656
xander254 <alexkinyanjui356@gmail.com>
2024-07-23 03:06:35 +0300

    A struct with elements about a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOH_H
+
+/**
+  * struct dog - details about a dog
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+  */
+
+#ifndef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
