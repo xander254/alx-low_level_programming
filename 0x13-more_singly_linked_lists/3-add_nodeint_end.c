7d410e733ed7d50090e86936806e68402b9bac67
xander254 <alexkinyanjui356@gmail.com>
2024-08-07 01:36:04 +0300

    refactored: moved the logic to populate the new node so that it is executed right after the node is created

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,6 +18,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -30,8 +32,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			temp = temp->next;
 		}
 		temp->next = new_node;
-		new_node->n = n;
-		new_node->next = NULL;
 	}
 	return (new_node);
 }
