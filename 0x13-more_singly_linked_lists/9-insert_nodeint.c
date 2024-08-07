ca68453f63b4835f728f994a4d4723d02ab8f94e
xander254 <alexkinyanjui356@gmail.com>
2024-08-07 23:48:36 +0300

    refactored

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,32 +14,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *new_node;
 	unsigned int count = 0;
 
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	temp = *head;
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 		count++;
 	}
-	if (idx > count)
+	if (temp == NULL)
 	{
+		free(new_node);
 		return (NULL);
+	
 	}
-	else
-	{
-		temp = *head;
-		while (idx == count)
-		{
-			temp = temp->next;
-			idx++;
-		}
-		new_node = (listint_t *)malloc(sizeof(listint_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->n = n;
-		new_node->next = temp->next;
-		temp->next = new_node;
-	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+
 	return (new_node);
 }
