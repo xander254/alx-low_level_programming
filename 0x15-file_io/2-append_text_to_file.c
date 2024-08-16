9ab19e079f9db429d3febd6b466850bd3c4aa0fb
xander254 <alexkinyanjui356@gmail.com>
2024-08-16 21:29:11 +0300

    refactor

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,12 +17,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	file_pointer = fopen(filename, "a");
+	if (file_pointer == NULL)
 	{
 		return (-1);
 	}
-	file_pointer = fopen(filename, "a");
-	if (file_pointer == NULL)
+	if (text_content == NULL)
 	{
 		return (-1);
 	}
