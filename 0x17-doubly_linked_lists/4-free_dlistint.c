31a591b8fbcca42e63650eb8e98f1623bf259ae6
xander254 <alexkinyanjui356@gmail.com>
2024-09-20 02:32:43 +0300

    a function that frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * free_dlistint -  a function that frees a dlistint_t list
+  * @head: the head of the list
+  */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node, *next_node;
+
+	if (head == NULL)
+	{
+		perror("The list is empty");
+		return;
+	}
+	current_node = head;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+}
