0a4d15099dc10ddfbbabf01c899bea01fcbad2e2
Alex Kinyanjui <61239695+xander254@users.noreply.github.com>
2024-10-14 23:12:29 +0300

    minor correction 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,32 +10,31 @@
 
 int main(void)
 {
-	int i;
+	int i = 0;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz ");
-		}
 		else
 		{
 			printf("%d", i);
-			if (i != 100)
+			if (i == 100)
 			{
-			printf(" ");
+				printf("\n");
 			}
 		}
+	printf(" ");
 	}
-	printf("\n");
-
 	return (0);
 }
