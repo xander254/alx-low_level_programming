a99ed41087817818befee0fa36b6a24efc2c26e2
xander254 <alexkinyanjui356@gmail.com>
2025-01-30 03:06:05 +0300

    Fix: syntax look and feel.. a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,19 +9,20 @@
   */
 int linear_search(int *array, size_t size, int value)
 {
-size_t i = 0;
+	size_t i = 0;
 
-if (array == NULL)
-{
-	return (-1);
-}
-
-for (i = 0; i < size; i++)
-{
-	if (array[i] == value)
+	if (array == NULL)
 	{
-		return (i);
+		return (-1);
 	}
-}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
 	return (-1);
 }
